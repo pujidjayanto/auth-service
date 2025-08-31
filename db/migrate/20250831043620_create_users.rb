0d6a67e54b60a660985eb0dc0404e45441dd0c3b
pujidjayanto <dedypuji@outlook.com>
2025-08-31 11:47:05 +0700

    chore: use explicit up and down for create user migration

--- db/migrate/20250831043620_create_users.rb
+++ db/migrate/20250831043620_create_users.rb
@@ -1,5 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
-  def change
+  def up
     create_table :users do |t|
       t.string :user_id
       t.string :password_digest
@@ -8,5 +8,9 @@ class CreateUsers < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
