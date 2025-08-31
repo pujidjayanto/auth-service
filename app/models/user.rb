bff02bd59ed32c022abc631da7d6162d446f4790
pujidjayanto <dedypuji@outlook.com>
2025-08-31 12:00:07 +0700

    fix: add validation for user_id and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :user_id, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :user_id,
+            presence: true,
+            uniqueness: true,
+            length: { in: 6..20 },
+            format: {
+              with: /\A[a-zA-Z0-9]+\z/,
+              message: "Incorrect character pattern"
+            }
+
+  validates :password,
+            presence: true,
+            length: { in: 8..20 },
+            format: {
+              with: /\A[!-~]+\z/,
+              message: "Incorrect character pattern"
+            }
+
+  validate :user_id_cannot_start_with_test
+
+  private def user_id_cannot_start_with_test
+    if user_id.present? && user_id&.start_with?("Test")
+      errors.add(:user_id, "cannot start with 'Test'")
+    end
+  end
 end
